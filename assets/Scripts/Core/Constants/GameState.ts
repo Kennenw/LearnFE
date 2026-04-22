221bce3a45468f23756aea72843c72f4769c6eaf
Kennenw <nguyenhuuphuc12323@gmail.com>
2026-04-22 11:43:04 +0700

    init project

---
+++ assets/Scripts/Core/Constants/GameState.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameState')
+export class GameState extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/Scripts/Systems/Input.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Input')
+export class Input extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
