4f2abe35cdae27fc495348a0b427622b9fec6f5b
Kennenw <nguyenhuuphuc12323@gmail.com>
2026-04-28 22:42:08 +0700

    update

--- assets/Scripts/Managers/LoadingManager.ts
+++ assets/Scripts/Managers/LoadingManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, director, ProgressBar, Node, Label } from 'cc';
+import { _decorator, Component, director, ProgressBar, Node, Label, Slider } from 'cc';
 import { SceneManager } from './SceneManager';
 const { ccclass, property } = _decorator;
 
@@ -7,6 +7,9 @@ export class LoadingManager extends Component {
 
     @property(ProgressBar)
     progressBar: ProgressBar
+
+    @property(Slider)
+    slider: Slider
 
     @property([Node])
     backgrounds: Node[] = [];
@@ -29,10 +32,13 @@ export class LoadingManager extends Component {
 
     protected start(): void {
         this.progressBar.progress = 0;
+        this.slider.progress = 0;
+        this.slider.enabled = false;
         const target = SceneManager.nextScene || "Lobby";
         director.preloadScene(target, (completed, total) => {
             const progress = completed / total;
             this.progressBar.progress = Math.max(this.progressBar.progress, progress);
+            this.slider.progress = Math.max(this.progressBar.progress, progress);
             this.loading.string = `Loading... ${Math.floor(this.progressBar.progress * 100)}%`
         }, () => {
             this.scheduleOnce(() => {
